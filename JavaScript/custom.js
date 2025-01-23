6684274ddf271c67b17bc80440f2db2ba7051d49
Ahmad <mianahmadaliali123@gmail.com>
2025-01-23 23:36:49 +0500

    I learn and Practice JavaScript from Chapter 69 to 75 and learn Objects proterties methods constractora and prototype

---
+++ JavaScript/custom.js
@@ -0,0 +1,17 @@
+const clearInput = () => {
+    document.getElementById("input").value = ""; // Clear the input field
+}
+
+const getInputValue = () => {
+    return document.getElementById("input").value; // Return the current value of the input field
+}
+function clearOutput(){
+    document.getElementById("output").innerHTML = "";
+}
+function getFieldValue() {
+    
+}
+function showTable() {
+    event.preventDefault()
+    let firstName = getFieldValue("firstName")
+}
